238c4f21f0c50437ba07197ee115bae13c54aa35
Alton Moore <aomoore3@gmail.com>
2016-06-19 23:40:47 -0500

    OK, it works rather crudely, but it works.  Improvements, such as proper scrolling of the text box, coming shortly.

--- client.js
+++ client.js
@@ -4,9 +4,27 @@ var express = require("express");
 var app     = express();
 var net     = require("net");
 var irc     = require("irc");
+var path    = require('path');  // This is so we don't have to specify the full path to the config file.
+
+var config_file = require(__dirname + "/" + "config.json");
+
+var last_channel_message = "";
+
+//--------------------------------------------------------------
+
+// This starts the Node server on the specified port.
+app.listen(config_file.port,function() {
+  console.log("Server started on port " + config_file.port);
+  });
 
 //----------------------------------------------------------
 // These are the Express.js routing functions.
+
+// This is the default, the client.html page.
+app.get('/',function(req,res) {
+  console.log("html requested");
+  res.sendFile(path.join(__dirname + '/client.html'));
+  });
 
 // This returns the entire config file to the web page so it can use some of the settings.
 app.get('/config_file', function(req,res) {
@@ -14,23 +32,26 @@ app.get('/config_file', function(req,res) {
   res.json(config_file);
   });
 
-// This is the default, the index.html page.
-app.get('/',function(req,res) {
-  console.log("html requested");
-  res.sendFile(path.join(__dirname + '/' + use_case_name + '-violations.html'));
+// This returns the last channel message whenever called, or blank for none.
+app.get('/last_channel_message', function(req,res) {
+  res.json({"lastChannelMessage": last_channel_message});
+  last_channel_message = "";
   });
 
-// This returns the last reported violation count whenever called.
-app.get('/current_violations', function(req,res) {
-  res.json({"currentViolationCount":current_violation_count / update_frequency});
-  });
+// This sends a message to the channel.
+app.get('/send_channel_message', function(req,res) {
+  var message_to_send = req.param('message');
+  console.log("Sending to server: " + message_to_send);
+  irc_client.say(config_file.ircChannel, message_to_send);
+  res.end("OK");
+});
 
 //-----------------------------------------------------------
 // Connect to IRC server now.
 
-var irc_client = new irc.Client('irc.alton-moore.net', 'AltonNode', {
+var irc_client = new irc.Client(config_file.ircServer, config_file.nickname, {
     debug: true,
-    channels: ['#alton', '#nkx']
+    channels: [config_file.ircChannel]
 });
 
 irc_client.addListener('error', function(message) {
@@ -46,15 +67,7 @@ irc_client.addListener('message', function(from, to, message) {
 
     if (to.match(/^[#&]/)) {
         // channel message
-        if (message.match(/hello/i)) {
-            irc_client.say(to, 'Hello there ' + from);
-        }
-        if (message.match(/dance/)) {
-            setTimeout(function() { irc_client.say(to, '\u0001ACTION dances: :D\\-<\u0001'); }, 1000);
-            setTimeout(function() { irc_client.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 2000);
-            setTimeout(function() { irc_client.say(to, '\u0001ACTION dances: :D/-<\u0001');  }, 3000);
-            setTimeout(function() { irc_client.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 4000);
-        }
+        last_channel_message = message;
     }
     else {
         // private message
@@ -73,3 +86,4 @@ irc_client.addListener('part', function(channel, who, reason) {
 irc_client.addListener('kick', function(channel, who, by, reason) {
     console.log('%s was kicked from %s by %s: %s', who, channel, by, reason);
 });
+
